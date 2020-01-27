861459e88b40b5758d6349867c7084ae855b198e
Hugo Burbelo <hburbelo@phone2action.com>
2020-01-26 21:11:52 -0500

    Add basic socket io connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Send current time to all connected clients
+function sendTime() {
+    io.sockets.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 10000);
+
+// Emit welcome message on connection
+io.sockets.on('connection', function(socket) {
+    socket.emit('welcome', { message: 'Welcome!' });
+
+    socket.on('i am client', function() {
+      console.log('received a message from the client.');
+    });
+});
+
+app.listen(3001);
