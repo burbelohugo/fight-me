2ab1f8b8c863cd6887921c0ad7da72ce0c156f5d
eric-robertson <e.robertson.js@gmail.com>
2020-01-26 21:10:06 -0500

    derpy figure drawn

--- frontend/main.js
+++ frontend/main.js
@@ -2,21 +2,57 @@ let socket = io ();
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext("2d");
 
-let commandMap = {
+let images = {
+    'standing' : document.getElementById('standing')
+}
+
+let singleActions = {
     'w' : 'jump',
     's' : 'crouch',
-    'a' : 'left',
-    'd' : 'right',
     ' ' : 'jump',
     'Enter' : 'attack'
 }
+let continuousActions = {
+    'a' : 'left',
+    'd' : 'right',
+}
 
 function keydown ( evt ) {
-    if ( commandMap[evt.key] )
-        socket.emit('doAction', { 'action' : commandMap[evt.key] } )
+    if ( singleActions[evt.key] )
+        socket.emit('doAction', { 'action' : singleActions[evt.key] } )
+    if ( continuousActions[evt.key] )
+        socket.emit('startAction', { 'action' : continuousActions[evt.key] } )
 }
 function keyup ( evt ){
+    if ( continuousActions[evt.key] )
+        socket.emit('stopAction', { 'action' : continuousActions[evt.key] } )
 }
 
 document.addEventListener( 'keydown', keydown )
 document.addEventListener( 'keyup', keyup )
+
+// Game Code
+
+let players = []
+
+socket.on ( 'registerPlayer', info => {
+    players[info.id] = info
+})
+
+socket.on( 'stateUpdate', data => {
+    data.players.forEach( player => {
+        players[player.id] = { ... players[player.id], ... player }
+    })
+})
+
+// Game Loop
+
+setInterval(() => {
+    
+    ctx.fillStyle = "#FFFFFF"
+    ctx.fillRect(0, 0, 800, 800);
+    
+    ctx.drawImage( images['standing'], 100, 100 )
+    
+
+}, 1000 / 60);
